27a7b4f209c2667c3f64f1e00b957f6600e58945
Kamalesh1512 <81355463+Kamalesh1512@users.noreply.github.com>
2023-08-07 21:21:41 +0530

    Update streamlit_app.py
    adding csv file called fruits list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas as pd
+
 
 streamlit.title("My parents new healthy diner")
 
@@ -9,3 +11,6 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacodo toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruits_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruits_list)
