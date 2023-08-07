46745b9e99fd773f3533e8cc7a957d90935f65cd
Kamalesh1512 <81355463+Kamalesh1512@users.noreply.github.com>
2023-08-07 21:24:34 +0530

    Update streamlit_app.py
    Creating  Some User Interaction

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,6 @@ streamlit.text('🥑🍞 Avacodo toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruits_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.multiselect('pick some fruits:' list(my_fruits_list.index)
 streamlit.dataframe(my_fruits_list)
